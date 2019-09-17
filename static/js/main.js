d6f1c186f5befaa923d7af1afb517132d3fd82f5
Pittosh Poulose <pittosh@citrusinformatics.com>
2019-09-17 10:48:18 +0530

    modified team member designation, SuiteCRM integartion on contact form
    modified team member designation, SuiteCRM integartion on contact form

--- static/js/main.js
+++ static/js/main.js
@@ -165,32 +165,91 @@ $(document).ready(function(){
     	 $('#email').css('display', 'none');
     	 e.preventDefault();
     });
-
-
-    $('#contactSubmit').on('click', function(e){
+    /*Function for creating unique access token */
+  	function uuidv4() {
+    	  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    	    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    	  )
+    }
+    $('#contactSubmit').click(function(e){
     	 var first_name = $('#contactName').val();
-         var email = $('#contactEmail').val();
+       var email = $('#contactEmail').val();
+       var subject = $('#contactSubject').val();
+       var message = $('#contactMessage').val();
 	     var o = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-	         if (first_name == '') {
-				$('#name').css('display', 'block');
-	      		return  false;
-	      		e.preventDefault();
-	    	 }
-	    	 if (email == '' || email.search(o) == -1 ) {
-	      		$('#email').css('display', 'block');
-	      		return  false;
-	      		 e.preventDefault();
-	    	 }
-	    	 if(first_name != ''){
-	          $('#name').css('display', 'none');
-	          return  true;  
-			 }
-			 if(email != ''){
-	          $('#email').css('display', 'none');
-	          return  true;  
- 		}
+       var uid = uuidv4();
+	     if (first_name == '') {
+          $('#name').css('display', 'block');
+              return  false;
+         }
+         else {
+          $('#name').css('display', 'none');
+         }
+         if (email == '' || email.search(o) == -1) {
+          $('#email').css('display', 'block');
+            return  false;
+         }
+         else {
+          $('#email').css('display', 'none');
+         }
+          var form = new FormData();
+          form.append("grant_type", "client_credentials");
+          form.append("client_id", "c395a3fe-0638-3616-d4dc-5d7f5d7729be");
+          form.append("client_secret", "K^&OOv#nvNTw");
+          var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://crm.netspective.com/Api/access_token",
+            "method": "POST",
+            "headers": {
+              "Accept": "application/vnd.api+json"
+            },
+            "processData": false,
+            "contentType": false,
+            "mimeType": "multipart/form-data",
+            "data": form
+          }
+    $.ajax(settings).done(function (response) {
+      var obj = $.parseJSON(response);
+      var access_token= obj.access_token;
+      var settings = {
+          "url": "https://crm.netspective.com/Api/V8/module",
+          "method": "POST",
+          "headers": {
+            "Accept": "application/vnd.api+json",
+            "Authorization": "Bearer "+access_token+"",
+            "Content-Type": "application/json"
+           },
+          "processData": false,
+          "data": "{\r\n  \"data\": {\r\n    \"type\": \"Contacts\",\r\n    \"id\": \""+uid+ "\",\r\n    \"attributes\": {\r\n     \"first_name\":\""+first_name+"\",\r\n     \"email1\":\""+email+"\"\r\n,\r\n     \"description\":\""+message+"\"\r\n,\r\n     \"lead_source\":\"Web Site\"\r\n,\r\n     \"title\":\""+subject+"\"\r\n   }\r\n  }\r\n}\r\n"
+        }
+      $.ajax(settings).done(function (response) {
+          var contactid = response.data.id;
+          var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://crm.netspective.com/Api/V8/module/Accounts/be63b2af-97c4-d409-4b20-5d7f5cb06641/relationships",
+            "method": "POST",
+            "headers": {
+              "Accept": "application/vnd.api+json",
+              "Authorization": "Bearer "+access_token+"",
+              "Content-Type": "application/json"
+            },
+            "processData": false,
+            "data": "{  \r\n   \"data\":{  \r\n         \"type\":\"Contacts\",\r\n         \"id\":\""+contactid+"\"\r\n\t    \r\n      }\r\n}"
+          }
+          $.ajax(settings).done(function (response) {
+            if(response.meta.message != ""){
+                var url="https://formspree.io/Gunjan.siroya@netspective.com"; 
+                $('#quickcontact').attr('action', url);
+                $('#quickcontact').submit();
+              }
+          });
+
+        }); 
 
     });
-  
 
+  });
+  
 });
